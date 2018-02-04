bf137e7146d80284c5e0a096b0f97a8bfdc32f79
Jonas Damtoft <jonasdamtoft@hotmail.com>
2018-02-04 13:58:53 +0100

    Fix typescript complaint

--- src/schemas/array.ts
+++ src/schemas/array.ts
@@ -9,18 +9,21 @@ export class ArraySchema<A> extends Schema<A[]> {
       if (Array.isArray(testObj)) {
         const arr: A[] = [];
         const results = testObj.map((e) => schema.validate(e));
-        return results.reduce((acc, cur, index) => {
-          if (cur.valid) {
-            arr[index] = cur.obj;
-            return acc;
-          } else {
-            const result = acc.valid
-              ? cur
-              : invalid(path, ...acc.errors.concat(cur.errors));
-            cur.errors.forEach((e) => e.path.unshift(index));
-            return result;
-          }
-        }, valid<A[]>(arr)) as ValidationResult<A[]>;
+        return results.reduce(
+          (acc, cur, index) => {
+            if (cur.valid) {
+              arr[index] = cur.obj;
+              return acc;
+            } else {
+              const result = acc.valid
+                ? cur
+                : invalid(path, ...acc.errors.concat(cur.errors));
+              cur.errors.forEach((e) => e.path.unshift(index));
+              return result;
+            }
+          },
+          valid<A[]>(arr) as ValidationResult<A[]>
+        );
       }
       return invalid(path, new TypeError(testObj, "array"));
     });
